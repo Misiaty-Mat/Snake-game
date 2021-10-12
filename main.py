3bff30170c455cae8d75da6304cddfa6f72fa2b1
Misiaty-Mat <m.a.s.m@interia.pl>
2021-10-12 16:39:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -40,15 +40,15 @@ while game_on:
 
     # Wall collision detector
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_on = False
-        scoreboard.game_over()
+        scoreboard.reset()
+        snake.reset()
 
     # tail collision detector
     for segment in snake.segments[1:]:
         # taking all segment apart from first one
         if snake.head.distance(segment) < 10:
-            game_on = False
-            scoreboard.game_over()
+            scoreboard.reset()
+            snake.reset()
 
 
 screen.exitonclick()
